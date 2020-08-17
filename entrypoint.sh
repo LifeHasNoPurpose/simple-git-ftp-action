b4877d9b2e1fe09523141b44bd54b3fa3474be21
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2020-08-17 10:51:31 +0200

    Probably final

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 
-git ftp push -f -vv --auto-init --user $user --passwd '$password' $git_ftp_params $server
+git ftp push -f -vv --auto-init --user $INPUT_USER --passwd "$INPUT_PASSWORD" $INPUT_GIT_INIT_PARAMS $INPUT_SERVER
 

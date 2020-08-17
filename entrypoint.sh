3cd3b62c9004649689861023aab40c0c3d14dece
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2020-08-17 10:56:05 +0200

    final most likely

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 
-git ftp push -f -vv --auto-init --user $INPUT_USER --passwd "$INPUT_PASSWORD" $INPUT_GIT_INIT_PARAMS $INPUT_SERVER
+git ftp push -f -v --auto-init --user $INPUT_USER --passwd "$INPUT_PASSWORD" $INPUT_GIT_INIT_PARAMS $INPUT_SERVER
 

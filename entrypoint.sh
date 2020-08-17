a358d346264d152d20659a1447f8ed22ca439a86
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2020-08-17 10:38:40 +0200

    another try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-git-ftp push -f -v --auto-init --user $user --passwd $password $git_ftp_params $server
+git ftp push -f -vv --auto-init --user $user --passwd '$password' $git_ftp_params $server
 
